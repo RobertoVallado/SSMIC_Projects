3f5bf6869a37274ebda789a3485cbe8d9a0f8ce1
roberto <roberto@github.com>
2021-05-08 14:21:37 -0400

    weather app complete with use of openweathermap.org/api

--- ModernJS/7_weatherApp/app.js
+++ ModernJS/7_weatherApp/app.js
@@ -1,64 +1,62 @@
 class UI {
-    constructor() {
-        //this.location = document.getElementById('w-location');
-        this.desc = document.getElementById('w-desc');
-        this.string = document.getElementById('w-string');
-        this.details = document.getElementById('w-details');
-        this.icon = document.getElementById('w-icon');
-    }
+  constructor() {
+    this.location = document.getElementById('w-location'); //location
+    this.details = document.getElementById('w-details'); //tempreature details
+    this.desc = document.getElementById('w-desc'); //description
+    this.icon = document.getElementById('w-icon'); // icon (image?)
+  }
 
-    // paint(weather) { //set values 
-    //     this.location.textContent = weather.display_location.full;
-    //     this.desc.textContent = weather.weather;
-    //     this.string.textContent = weather.temperature_string;
-    //     this.icon.setAttribute('src', weather.icon_url);
-    // }
+  paint(weather) { //set values
+    this.location.textContent = weather.name;
+    this.details.textContent = `Temperature is ${weather.main.temp} Celcius.`;
+    this.desc.textContent = weather.weather[0].description;
+    this.icon.setAttribute('src', `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+  }
 }
 
 class Weather {
-    constructor(city) {
-        this.apiKey = '';//api ky
-        this.city = city;
-    }
+  constructor(city) {
+    this.apiKey = ''; //api ky
+    //get a key from: https://openweathermap.org/api
+    this.city = city;
+  }
 
-    //fetch API
-    async getWeather() {
-        //const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Boston&appid=${this.apiKey}`)
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}`)
-        const responseData = await response.json();
+  //fetch API
+  async getWeather() {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=${this.apiKey}`)
+    const responseData = await response.json();
 
-        console.log(responseData);
-        return responseData;
-    }
+    return responseData;
+  }
 
-    //change location
-    changeLocation(city) {
-        this.city = city;
-    }
+  //change location
+  changeLocation(city) {
+    this.city = city;
+  }
 }
 
 //storage class
 class Storage {
-    constructor() {
-        this.city;
-        this.defaultCity = 'Mountain View';
+  constructor() {
+    this.city;
+    this.defaultCity = 'Toronto';
+  }
+
+  getLocationData() {
+    if (localStorage.getItem('city') === null) {
+      this.city = this.defaultCity;
+    } else {
+      this.city = localStorage.getItem('city');
     }
 
-    getLocationData() {
-        if (localStorage.getItem('city') === null) {
-            this.city = this.defaultCity;
-        } else {
-            this.city = localStorage.getItem('city');
-        }
-
-        return {
-            city: this.city
-        }
+    return {
+      city: this.city
     }
+  }
 
-    setLocationData(city) {
-        localStorage.setItem('city', city);
-    }
+  setLocationData(city) {
+    localStorage.setItem('city', city);
+  }
 }
 
 const storage = new Storage();
@@ -75,15 +73,13 @@ document.getElementById('w-change-btn').addEventListener('click', (e) => {
     storage.setLocationData(city);
 
     getWeather();
-
-    $('#locModal').modal('hide');
 });
 
 function getWeather() {
-    weather.getWeather()
-        .then(results => {
-            console.log(results);
-            //ui.paint(results);
-        })
-        .catch(err => console.log(err));
+  weather.getWeather()
+    .then(results => {
+      console.log(results)
+      ui.paint(results);
+    })
+    .catch(err => console.log(err));
 }
